6e2bbb7a9eef2931e70d972e29f206078aafd4f0
AlbertoMessina <alberto96.messina@gmail.com>
2021-12-06 16:01:48 +0100

    storybook second build

---
+++ storybook-static/assets/data/card-mock.ts
@@ -0,0 +1,142 @@
+import { StorybookCard } from "src/app/models/models";
+
+export const SmallStorybookCard: StorybookCard[] =
+    [
+        {
+            'card': {
+                'id': '0',
+                'title': 'First Impact ',
+                'type': 'small-card',
+                'imageSrc': '../../assets/firstimpact.jpeg',
+                'altImageSrc': 'First Impact',
+            },
+            'actionbar': {
+                'position': 'center',
+                'rounded': false,
+                'type': 'cart'
+            }
+        },
+
+        {
+            'card': {
+                'id': '1',
+                'title': 'Second Impact ',
+                'type': 'small-card',
+                'imageSrc': '../../assets/secondImpact.jpeg',
+                'altImageSrc': 'First Impact',
+            },
+            'actionbar': {
+                'position': 'center',
+                'rounded': false,
+                'type': 'cart'
+            }
+        },
+        {
+            'card': {
+                'id': '2',
+                'title': 'Third Impact ',
+                'type': 'small-card',
+                'imageSrc': '../../assets/thirdImpact.jpeg',
+                'altImageSrc': 'First Impact',
+            },
+            'actionbar': {
+                'position': 'center',
+                'rounded': false,
+                'type': 'cart'
+            }
+        },
+    ];
+
+export const MediumStorybookCard: StorybookCard[] =
+    [
+        {
+            'card': {
+                'id': '0',
+                'title': 'First Impact ',
+                'type': 'medium-card',
+                'imageSrc': '../../assets/firstimpact.jpeg',
+                'altImageSrc': 'First Impact',
+            },
+            'actionbar': {
+                'position': 'right',
+                'rounded': true,
+                'type': 'social'
+            }
+        },
+
+        {
+            'card': {
+                'id': '1',
+                'title': 'Second Impact ',
+                'type': 'medium-card',
+                'imageSrc': '../../assets/secondImpact.jpeg',
+                'altImageSrc': 'First Impact',
+            },
+            'actionbar': {
+                'position': 'center',
+                'rounded': true,
+                'type': 'social'
+            }
+        },
+        {
+            'card': {
+                'id': '2',
+                'title': 'Third Impact ',
+                'type': 'medium-card',
+                'imageSrc': '../../assets/thirdImpact.jpeg',
+                'altImageSrc': 'First Impact',
+            },
+            'actionbar': {
+                'position': 'left',
+                'rounded': true,
+                'type': 'social'
+            }
+        },
+    ];
+
+export const LargeStorybookCard: StorybookCard[] =
+    [
+        {
+            'card': {
+                'id': '0',
+                'title': 'First Impact ',
+                'type': 'large-card',
+                'imageSrc': '../../assets/firstimpact.jpeg',
+                'altImageSrc': 'First Impact',
+            },
+            'actionbar': {
+                'position': 'center',
+                'rounded': false,
+                'type': 'cart'
+            }
+        },
+
+        {
+            'card': {
+                'id': '1',
+                'title': 'Second Impact ',
+                'type': 'large-card',
+                'imageSrc': '../../assets/secondImpact.jpeg',
+                'altImageSrc': 'First Impact',
+            },
+            'actionbar': {
+                'position': 'center',
+                'rounded': false,
+                'type': 'cart'
+            }
+        },
+        {
+            'card': {
+                'id': '2',
+                'title': 'Third Impact ',
+                'type': 'large-card',
+                'imageSrc': '../../assets/thirdImpact.jpeg',
+                'altImageSrc': 'First Impact',
+            },
+            'actionbar': {
+                'position': 'center',
+                'rounded': false,
+                'type': 'cart'
+            }
+        },
+    ];
